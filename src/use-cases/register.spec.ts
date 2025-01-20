a3ac54c00ff0636838ee6d59f7d3f0cf73dba2f9
paulohdb <paulinhogt6@gmail.com>
2025-01-20 16:48:11 -0400

    Creating the first Unit Test Example

---
+++ src/use-cases/register.spec.ts
@@ -0,0 +1,33 @@
+import { expect, describe, it } from 'vitest'
+import { RegisterUseCase } from './register'
+import { compare } from 'bcryptjs'
+
+describe('Register Use Case', () => {
+  it('should hash user password upon registration', async () => {
+    const registerUseCase = new RegisterUseCase({
+      async findByEmail(email) {
+        return null
+      },
+
+      async create(data) {
+        return {
+          id: 'user-1',
+          name: data.name,
+          email: data.email,
+          password_hash: data.password_hash,
+          created_at: new Date(),
+        }
+      },
+    })
+
+    const { user } = await registerUseCase.execute({
+      name: 'John Doe',
+      email: 'johndoe@mail.com',
+      password: '12345678',
+    })
+
+    const isPasswordHashed = await compare('12345678', user.password_hash)
+
+    expect(isPasswordHashed).toBe(true)
+  })
+})
